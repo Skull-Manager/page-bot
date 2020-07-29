8ebd2f01a052323f3f106931ac53191365c6ade9
Skull - Manager <51505046+Skull-Manager@users.noreply.github.com>
2020-07-29 14:18:01 +0300

    Update and rename src/bot.php to public_html/bot.php

--- public_html/bot.php
+++ public_html/bot.php
@@ -2,7 +2,7 @@
 require ('vk_api.php'); // подключение библиотеки
 require ('helper.php'); // подключения скрипта помощника 
 
-$data = $_POST['out']; // ловим данные от сервера
+$data = json_decode ($_POST['out']);; // ловим данные от сервера
 
 $token = ""; // ваш токен от кейт мобаил
 $v = "5.85"; // можно указать 5.120 (современная версия)
@@ -10,7 +10,8 @@ $v = "5.85"; // можно указать 5.120 (современная верс
 $vk = new vk_api($token, $v); 
 $skull = new Skull ($vk);
 
-$data_get = $vk->request('messages.search', ['q' => $data['text'] ]) ['items'][0]; // получение инфы о сообщении (костыль)
+
+$data_get = $vk->request('messages.search', ['q' => $data->text ]) ['items'][0]; // получение инфы об сообщении (костыль)
 
 // тащим данные от костыля
 $peer_id    = $data_get['peer_id'];
@@ -20,14 +21,14 @@ $c_mes_id   = $data_get['conversation_message_id'];
 
 // ниже стряпано на быструю руку
 
-if ($data['method'] == 'skullEdit') { // редактирует сообщение (/апи е)
-    $vk->request('messages.edit', ['peer_id' => $peer_id, 'message' => $data['text'], 'message_id' => $message_id]);
-} elseif ($data['method'] == 'skullSend') { // отправляет сообщение с удалением старого (/апи с)
+if ($data->method == 'skullEdit') { // редактирует сообщение (/апи е)
+    $vk->request('messages.edit', ['peer_id' => $peer_id, 'message' => $data->text, 'message_id' => $message_id]);
+} elseif ($data->method == 'skullSend') { // отправляет сообщение с удалением старого (/апи с)
     $vk->request('messages.delete', ['message_ids' => $message_id, 'delete_for_all' => 1]);
-    $vk->sendMessage($peer_id, $data['text']);
-} elseif ($data['method'] == 'skullCheck') { // проверка сервера
+    $vk->sendMessage($peer_id, $data->text);
+} elseif ($data->method == 'skullCheck') { // проверка сервера
     echo $c_mes_id;
-} elseif ($data['method'] == 'skullDelDogs') { // удаление собак
+} elseif ($data->method == 'skullDelDogs') { // удаление собак
     $request = $skull->deleteDogs();
     $vk->request('messages.edit', ['peer_id' => $peer_id, 'message' => $request, 'message_id' => $message_id]);
 }
