aee034f514a76eaea91e5a0bf755d817ceab6d25
Skull - Manager <51505046+Skull-Manager@users.noreply.github.com>
2020-08-01 22:53:18 +0300

    Update bot.php

--- public_html/bot.php
+++ public_html/bot.php
@@ -10,15 +10,23 @@ $data->method - метод от skull-а ($method)
 $data->text   - сообщение из чата
 */
 
-    
+$message = mb_strtolower ($data->text);
+
 if ($method == 'skullSend') {
-    $skull->skullSend ($message_id, $peer_id, $data->text);
+    
+    $my_func = mb_substr ($message,0,1);
+    
+    if ($my_func == 'н') { // н == напиши
+        $skull->skullSend ($message_id, $peer_id, mb_substr ($data->text, 2));
+    }  
+    
+    if ($my_func == 'е') { // е = edit = отредактируй
+        $vk->request('messages.edit', ['peer_id' => $peer_id, 'message' => mb_substr ($data->text, 2), 
+        'message_id' => $message_id]);
+    }  
+    
+    if ($my_func == 'д') { // д = delete = удалить (собак)
+        $skull->deleteDogs($message_id, $peer_id);
+    }  
+    
 }   
-
-if ($method == 'skullEdit') {
-    $vk->request('messages.edit', ['peer_id' => $peer_id, 'message' => $data->text, 'message_id' => $message_id]);
-} 
-
-if ($method == 'skullDelDogs') {
-    $skull->deleteDogs($message_id, $peer_id);
-}
