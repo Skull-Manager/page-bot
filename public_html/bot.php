e9b5cd71b15383a1e79abf50b70ed85d677508a7
Skull - Manager <51505046+Skull-Manager@users.noreply.github.com>
2020-08-05 20:09:19 +0300

    Update bot.php

--- public_html/bot.php
+++ public_html/bot.php
@@ -5,6 +5,7 @@ require_once 'src/connect.php'; // подключение всех данных
 $peer_id      - id чата
 $message_id   - id сообщения
 $c_mes_id     - id глобальный
+$reply_id     - ид юзера, чье смс было пересланно
 
 $data->method - метод от skull-а ($method)
 $data->text   - сообщение из чата
@@ -14,23 +15,37 @@ $message = mb_strtolower ($data->text);
 
 if ($method == 'skullSend') {
     
-    $my_func = mb_substr ($message,0,1);
+    $func = mb_substr ($message, 0, 1); // получение 1-й буквы
     
-    if ($my_func == 'н') { // н == напиши
+    if ($message == 'чистка') { // удаление своих сообщений
+        $skull->skullDelMyMsg ($peer_id);
+    }
+    
+    if ($message == 'чистка от') { // удаление от пользователя
+        if (!empty ($reply_id)) {
+            $skull->skullDelFromMsg ($peer_id, $reply_id, $message_id);
+        }
+    }
+    
+    if ($func == 'н') { // н == напиши
         $skull->skullSend ($message_id, $peer_id, mb_substr ($data->text, 2));
     }  
     
-    if ($my_func == 'е') { // е = edit = отредактируй
+    if ($func == 'е') { // е = edit = отредактируй
         $vk->request('messages.edit', ['peer_id' => $peer_id, 'message' => mb_substr ($data->text, 2), 
         'message_id' => $message_id]);
     }  
     
-    if ($my_func == 'д') { // д = delete = удалить (собак)
+    if ($func == 'д') { // д = delete = удалить (собак)
         $skull->deleteDogs($message_id, $peer_id);
     }  
     
-    if ($my_func == 'с') { // с = статус
+    if ($func == 'с') { // с = статус
         $skull->setStatus ($message_id, $peer_id, mb_substr ($data->text, 2));
     } 
     
-}   
+    if ($func == 'п') { // произвольный чат
+        $skull->skullArbitrary ($data->text);
+    }
+    
+}  
